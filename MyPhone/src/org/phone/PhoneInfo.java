56fc61c1655897856c832eb749693c884baa16b8
SureshGithubWorks <124015641+SureshGithubWorks@users.noreply.github.com>
2023-03-03 08:11:51 +0530

    Add files via upload

---
+++ MyPhone/src/org/phone/PhoneInfo.java
@@ -0,0 +1,34 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		System.out.println("iphone");
+
+	}
+	private void phoneMieiNum() {
+		System.out.println("1801000000007876");
+
+	}
+	private void camera() {
+		System.out.println("64Mp");
+
+	}
+	private void storage() {
+		System.out.println("128gb");
+
+	}
+	private void osName() {
+		System.out.println("ios 10");
+
+	}
+	public static void main(String[] args) {
+		//class name object = new class name;
+		PhoneInfo n = new PhoneInfo();
+		n.phoneName();
+		n.phoneMieiNum();
+		n.camera();
+		n.storage();
+		n.osName();
+	}
+
+}
